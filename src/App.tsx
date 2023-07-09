b9a8a0e74d831ea1fce186953ef3f5ec6b65b6ca
KanSars <kansar13@yandex.ru>
2023-07-09 14:52:43 +0400

    feat: func CreateProduct added

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,27 @@ import { Loader } from './components/Loader';
 import { Modal } from './components/Modal';
 import { Products } from './components/Products';
 import { useProducts } from './hooks/useProducts';
+import { useState } from 'react';
 
 function App() {
-  const { loading, error, products} = useProducts();
+  const { loading, error, products } = useProducts();
+  const [modal, setModal] = useState<boolean>(false);
 
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      {loading && <Loader />}
-      <Products products={products}/>
-      {error && <ErrorMessage error={error}/>}
+      { loading && <Loader /> }
+      { error && <ErrorMessage error={error} /> }
 
-      <Modal title='Create new product'>
-        <CreateProduct />
-      </Modal>
+      <Products products={products} />
+
+      { modal && <Modal title='Create new product' onClose={() => setModal(false)}>
+        <CreateProduct onCreate={() => setModal(false)} />
+      </Modal> }
+
+      <button className='fixed top-5 right-5 rounded-full bg-green-700 text-white text-1xl px-4 py-2'
+        onClick={() => setModal(prev => !prev)}>
+        Добавить товар
+      </button>
     </div>
   );
 }
--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,17 +1,75 @@
-const CreateProduct = () => {
-    return (
-        <form action="">
-            <input 
-                type="text"
-                className="border py-2 px-4 mb-2 w-full outline-0"
-                placeholder="Enter product title..."
-            />
-            <button className="py-2 px-4 border bg-yellow-400 hover:text-white">Create</button>
-            
-        </form>
+import React, { useState } from "react";
+import { IProduct } from "../models";
+import { ErrorMessage } from "./ErrorMessage";
+
+const newProductData: IProduct = {
+  title: 'test product',
+  price: 13.5,
+  description: 'lorem ipsum set',
+  image: 'https://i.pravatar.cc',
+  category: 'electronic',
+  rating: {
+    rate: 42,
+    count: 10,
+  }
+}
+
+interface CreateProductProps {
+  onCreate: () => void
+}
+
+const postFetch = (newProductData: IProduct) => {
+  return fetch('https://fakestoreapi.com/products', {
+    method: "POST",
+    body: JSON.stringify(
+      newProductData
     )
+  })
+    .then(res => res.json())
+}
+
+const CreateProduct = ({ onCreate }: CreateProductProps) => {
+  const [value, setValue] = useState<string>('');
+  const [validationError, setValidError] = useState<string>('');
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  }
+
+  const submitHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setValidError('');
+
+    if (value.trim().length === 0) {
+      setValidError('Please enter valid title...');
+      return;
+    }
+
+    const response = await postFetch(newProductData);
+    if (response) {
+      onCreate();
+    }
+    
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        className="border py-2 px-4 mb-2 w-full outline-0"
+        placeholder="Enter product title..."
+        onChange={changeHandler}
+        value={value}
+      />
+
+      {validationError && <ErrorMessage error={validationError}/>}
+
+      <button className="py-2 px-4 border bg-yellow-400 hover:text-white">Create</button>
+
+    </form>
+  )
 }
 
 export {
-    CreateProduct,
+  CreateProduct,
 }
